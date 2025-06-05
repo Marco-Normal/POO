b4979d64cc2b4524a6c11f61486c37afc4276592
Marco Túlio <marcotulio2709@usp.br>
2025-06-04 22:11:13 -0300

    Iteração dentro do menu, falta entrar no curso

--- scrapper.py
+++ scrapper.py
@@ -1,13 +1,39 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
 
 
 class JupiterScrapper:
     def __init__(self):
-        self.driver = webdriver.Firefox()
+        self.driver = webdriver.Firefox(
+            service=FirefoxService(GeckoDriverManager().install())
+        )
 
     def scrape(self):
         self.driver.get(
             "https://uspdigital.usp.br/jupiterweb/jupCarreira.jsp?codmnu=8275"
         )
+        campo_unidade = self.driver.find_element(By.ID, "comboUnidade")
+        dropdown_unidade = Select(campo_unidade)
+        enviar = self.driver.find_element(By.ID, "enviar")
+        grade_horaria = self.driver.find_element(By.ID, "step4-tab")
+        buscas = self.driver.find_element(By.ID, "step1-tab")
+        for i in range(len(dropdown_unidade.options)):
+            dropdown_unidade.select_by_index(i)
+            campo_curso = self.driver.find_element(By.ID, "comboCurso")
+            dropdown_curso = Select(campo_curso)
+            for j in range(len(dropdown_curso.options)):
+                dropdown_curso.select_by_index(j)
+
+
+def main():
+    scrapper = JupiterScrapper()
+    scrapper.scrape()
+
+
+if __name__ == "__main__":
+    main()
